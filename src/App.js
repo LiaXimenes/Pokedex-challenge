109a401a043226a2d96172c59c06c7b25fa57187
Lia Ximenes <ximeneslia@gmail.com>
2023-02-13 00:36:08 -0300

    environment ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "../src/assets/images/logo-pokedex.png";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={logo} alt="Logo"></img>
     </div>
   );
 }
-
-export default App;
